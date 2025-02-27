5610e3048b049eddbae18cded2f3ac379251b6d7
movieboxc17 <128537617+movieboxc17@users.noreply.github.com>
2025-02-27 10:39:36 +0100

    Create quizzes.js

---
+++ quizzes.js
@@ -0,0 +1,137 @@
+const quizzes = {
+    html: {
+        title: "HTML Quiz",
+        questions: [
+            {
+                question: "What does HTML stand for?",
+                choice1: "Hypertext Markup Language",
+                choice2: "Hypertext Markdown Language",
+                choice3: "Hyperloop Machine Language",
+                choice4: "Helicopters Terminals Motorboats Lamborginis",
+                answer: 1
+            },
+            {
+                question: "Which HTML tag is used for creating a line break?",
+                choice1: "<lb>",
+                choice2: "<break>",
+                choice3: "<br>",
+                choice4: "<newline>",
+                answer: 3
+            },
+            {
+                question: "Which HTML element is used to define an image?",
+                choice1: "<image>",
+                choice2: "<img>",
+                choice3: "<picture>",
+                choice4: "<src>",
+                answer: 2
+            },
+            {
+                question: "Which HTML element is used to define important text?",
+                choice1: "<important>",
+                choice2: "<strong>",
+                choice3: "<bold>",
+                choice4: "<em>",
+                answer: 2
+            },
+            {
+                question: "Which HTML attribute is used to specify a unique id for an HTML element?",
+                choice1: "class",
+                choice2: "unique",
+                choice3: "id",
+                choice4: "identifier",
+                answer: 3
+            }
+        ]
+    },
+    css: {
+        title: "CSS Quiz",
+        questions: [
+            {
+                question: "What does CSS stand for?",
+                choice1: "Colorful Style Sheets",
+                choice2: "Computer Style Sheets",
+                choice3: "Creative Style Sheets",
+                choice4: "Cascading Style Sheets",
+                answer: 4
+            },
+            {
+                question: "Which property is used to change the background color?",
+                choice1: "color",
+                choice2: "bgcolor",
+                choice3: "background-color",
+                choice4: "background",
+                answer: 3
+            },
+            {
+                question: "How do you add a comment in CSS?",
+                choice1: "// this is a comment",
+                choice2: "' this is a comment",
+                choice3: "/* this is a comment */",
+                choice4: "<!-- this is a comment -->",
+                answer: 3
+            },
+            {
+                question: "Which CSS property is used to change the text color of an element?",
+                choice1: "text-color",
+                choice2: "color",
+                choice3: "font-color",
+                choice4: "text-style",
+                answer: 2
+            },
+            {
+                question: "Which CSS property controls the text size?",
+                choice1: "text-size",
+                choice2: "font-style",
+                choice3: "font-size",
+                choice4: "text-style",
+                answer: 3
+            }
+        ]
+    },
+    javascript: {
+        title: "JavaScript Quiz",
+        questions: [
+            {
+                question: "Inside which HTML element do we put the JavaScript?",
+                choice1: "<javascript>",
+                choice2: "<js>",
+                choice3: "<scripting>",
+                choice4: "<script>",
+                answer: 4
+            },
+            {
+                question: "How do you create a function in JavaScript?",
+                choice1: "function = myFunction()",
+                choice2: "function myFunction()",
+                choice3: "function:myFunction()",
+                choice4: "createMyFunction()",
+                answer: 2
+            },
+            {
+                question: "How do you call a function named 'myFunction'?",
+                choice1: "call myFunction()",
+                choice2: "call function myFunction()",
+                choice3: "myFunction()",
+                choice4: "execute myFunction()",
+                answer: 3
+            },
+            {
+                question: "How to write an IF statement in JavaScript?",
+                choice1: "if i = 5 then",
+                choice2: "if i == 5 then",
+                choice3: "if (i == 5)",
+                choice4: "if i = 5",
+                answer: 3
+            },
+            {
+                question: "How does a FOR loop start?",
+                choice1: "for (i = 0; i <= 5; i++)",
+                choice2: "for (i <= 5; i++)",
+                choice3: "for i = 1 to 5",
+                choice4: "for (i = 0; i <= 5)",
+                answer: 1
+            }
+        ]
+    }
+};
